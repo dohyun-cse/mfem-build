d95b051c036cf0e22a3c0e5dc900444163ab0fbe
Dohyun Kim <dohyun_kim@brown.edu>
2025-04-21 20:56:04 -0400

    gslib

--- build-gslib.sh
+++ build-gslib.sh
@@ -1,28 +1,41 @@
 force_rebuild=false
 BUILD_TYPE="RELEASE"
-while getopts ":fg" opt; do
+while getopts ":g" opt; do
   case ${opt} in
-    f )
-      force_rebuild=true
-      ;;
     g )
       BUILD_TYPE="DEBUG"
+      echo "Building in debug mode..."
       ;;
     * )
-      echo "Usage: $0 [-f]"
+      echo "Usage: $0 [-g]"
       exit 1
       ;;
   esac
 done
 
-if $force_rebuild; then
-  echo "Rebuilding from scratch..."
-  rm -rf build
-  mkdir build
-  cmake -DMFEM_DEBUG=YES -DMFEM_USE_MPI=YES -DMFEM_USE_METIS_5=YES -DMETIS_DIR=../metis-5.1.0 -DMFEM_USE_GSLIB=YES -DGSLIB_DIR=../gslib/build -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -S . -B build
-  rm compile_commands.json
-  ln -s build/compile_commands.json .
-fi
+echo "Rebuilding from scratch..."
+export PETSC_DIR=$HOME/mfem/petsc-install
+export PETSC_ARCH=""
+make \
+  MFEM_USE_PETSC=YES \
+  PETSC_DIR=$PETSC_DIR \
+  PETSC_ARCH="" \
+  MFEM_USE_MPI=YES \
+  HYPRE_DIR=$PETSC_DIR \
+  MFEM_USE_METIS_5=YES \
+  METIS_DIR=$PETSC_DIR \
+  ParMETIS_DIR=$PETSC_DIR \
+  ScaLAPACK_DIR=$PETSC_DIR/../petsc/arch-darwin-c-opt/externalpackages/git.scalapack/petsc-build \
+  MFEM_USE_MUMPS=YES \
+  MUMPS_DIR=$PETSC_DIR \
+  MFEM_USE_GSLIB=YES \
+  GSLIB_DIR=../gslib/build \
+  MFEM_USE_SUITESPARSE=YES \
+  SUITESPARSE_DIR=$PETSC_DIR \
+  -j8
 
-cd build
-make -j8
+cat > .clangd <<EOF
+CompileFlags:
+  CompilationDatabase: build
+  Add: [-DMFEM_CONFIG_FILE="${PWD}/build/config/_config.hpp"]
+EOF
